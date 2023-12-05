6ce565cd20e02bbbb8c60374eedbbf2559896298
Jake Seunghyun Cho <78609676+SeunghyunCho22@users.noreply.github.com>
2023-12-05 21:43:47 +0900

    Add files via upload

---
+++ banking/src/system/main_Class.java
@@ -0,0 +1,110 @@
+package system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class main_Class extends JFrame implements ActionListener {
+    JButton b1,b2,b3,b4,b5,b6,b7;
+    String pin;
+    main_Class(String pin){
+        this.pin = pin;
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/atm2.png"));
+        Image i2 = i1.getImage().getScaledInstance(1550,830,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel l3 = new JLabel(i3);
+        l3.setBounds(0,0,1550,830);
+        add(l3);
+
+        JLabel label = new JLabel("원하시는 기능을 터치해주세요.");
+        label.setBounds(430,180,700,35);
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("System",Font.BOLD,28));
+        l3.add(label);
+
+        b1 = new JButton("예금");
+        b1.setForeground(Color.BLACK);
+        b1.setBackground(new Color(65,125,128));
+        b1.setBounds(410,274,150,35);
+        b1.addActionListener(this);
+        l3.add(b1);
+
+        b2 = new JButton("현금 인출");
+        b2.setForeground(Color.BLACK);
+        b2.setBackground(new Color(65,125,128));
+        b2.setBounds(700,274,150,35);
+        b2.addActionListener(this);
+        l3.add(b2);
+
+        b3 = new JButton("출금");
+        b3.setForeground(Color.black);
+        b3.setBackground(new Color(65,125,128));
+        b3.setBounds(410,318,150,35);
+        b3.addActionListener(this);
+        l3.add(b3);
+
+        b4 = new JButton("입출금 내역 명세서");
+        b4.setForeground(Color.black);
+        b4.setBackground(new Color(65,125,128));
+        b4.setBounds(700,318,150,35);
+        b4.addActionListener(this);
+        l3.add(b4);
+
+        b5 = new JButton("비밀번호 변경");
+        b5.setForeground(Color.black);
+        b5.setBackground(new Color(65,125,128));
+        b5.setBounds(410,362,150,35);
+        b5.addActionListener(this);
+        l3.add(b5);
+
+        b6 = new JButton("잔액 조회");
+        b6.setForeground(Color.black);
+        b6.setBackground(new Color(65,125,128));
+        b6.setBounds(700,362,150,35);
+        b6.addActionListener(this);
+        l3.add(b6);
+
+        b7 = new JButton("종료하기");
+        b7.setForeground(Color.black);
+        b7.setBackground(new Color(65,125,128));
+        b7.setBounds(700,406,150,35);
+        b7.addActionListener(this);
+        l3.add(b7);
+
+        setLayout(null);
+        setSize(1550,1080);
+        setLocation(0,0);
+        setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==b1){
+           new Deposit(pin);
+            setVisible(false);
+        }else if (e.getSource()==b7){
+            System.exit(0);
+        } else if (e.getSource()==b2) {
+            new Withdrawl(pin);
+            setVisible(false);
+        } else if (e.getSource()==b6) {
+            new BalanceEnquriy(pin);
+            setVisible(false);
+        } else if (e.getSource()==b3) {
+            new FastCash(pin);
+            setVisible(false);
+        } else if (e.getSource()==b5) {
+            new Pin(pin);
+            setVisible(false);
+        } else if (e.getSource()==b4) {
+            new mini(pin);
+        }
+    }
+
+    public static void main(String[] args) {
+        new main_Class("");
+    }
+}
